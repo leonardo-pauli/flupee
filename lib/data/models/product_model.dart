e4b97a78bd2552e92171790ef4b934189f95e410
leonardo-pauli <leonardopaulisantos@gmail.com>
2025-11-25 09:52:04 -0300

    feat(data): criando product e productModel para conversao JSON

---
+++ lib/data/models/product_model.dart
@@ -0,0 +1,34 @@
+import 'package:flupee/domain/entities/product.dart';
+
+class ProductModel extends Product {
+  ProductModel({
+    required super.id,
+    required super.title,
+    required super.description,
+    required super.price,
+    required super.thumbnail,
+    required super.category,
+  });
+
+  factory ProductModel.fromjson(Map<String, dynamic> json) {
+    return ProductModel(
+      id: json['id'],
+      title: json['title'],
+      description: json['description'] ?? '',
+      price: json['price' as num].toDouble(),
+      thumbnail: json['thumbnail'],
+      category: json['category'],
+    );
+  }
+
+  Map<String, dynamic> tojson() {
+    return {
+      'id': id,
+      'title': title,
+      'description': description,
+      'price': price,
+      'thumbnail': thumbnail,
+      'category': category,
+    };
+  }
+}
---
+++ lib/domain/entities/product.dart
@@ -0,0 +1,17 @@
+class Product {
+  final int id;
+  final String title;
+  final String description;
+  final double price;
+  final String thumbnail;
+  final String category;
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.thumbnail,
+    required this.category,
+  });
+}
