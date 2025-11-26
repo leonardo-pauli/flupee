2e037087924f3ed9e54e4941a6f6bfe0f549dcdb
leonardo-pauli <leonardopaulisantos@gmail.com>
2025-11-26 09:46:33 -0300

    feat(data): adicionando repositorio para busca de produtos da api

---
+++ lib/data/repositories/product_repository.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:flupee/data/models/product_model.dart';
+import 'package:flupee/domain/entities/product.dart';
+import 'package:http/http.dart' as http;
+
+
+class ProductRepository {
+  final String baseUrl = 'https://dummyjson.com';
+
+  Future<List<Product>> getProducts() async {
+    try{
+      final  response = await http.get(Uri.parse('$baseUrl/products'));
+
+      if (response.statusCode == 200) {
+
+        final Map<String, dynamic> jsonResponse = json.decode(response.body);
+
+        final List<dynamic> productsList = jsonResponse['products'];
+
+        return productsList.map((e) => ProductModel.fromjson(e)).toList();
+      }else{
+      throw Exception('Erro ao carregar os produtos. Código: ${response.statusCode}')
+      }
+    }
+  }
+}
